460765d4e624db2d11af0f658282666db878add4
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:45:30 -0700

    Initial Commit

---
+++ scroll-view/scroll_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  scroll_viewApp.swift
+//  scroll-view
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct scroll_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
