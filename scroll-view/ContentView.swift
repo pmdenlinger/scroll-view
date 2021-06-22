bdc942411e8dc94c4987de355b4e1a950d78117e
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:52:31 -0700

    ScrollableView code added.

--- scroll-view/ContentView.swift
+++ scroll-view/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+//        .vertical gives vertical scroll
+//        .horizontal gives horizontal scroll
+        ScrollView(.vertical, showsIndicators: false, content: {
+            
+//            Scrollable content
+            VStack(spacing: 20){
+                ForEach(1...30, id: \.self) {index in
+                    Text("Text Value =\(index)")
+                }
+            }
+        })
+        
     }
 }
 
